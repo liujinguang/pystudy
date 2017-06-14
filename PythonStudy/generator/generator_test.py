23a5602bc4914963c614b6539fd0ff506289d6a1
jinguang.liu <jinguang.liu@qq.com>
2017-06-14 12:38:00 -0400

    update generator

--- PythonStudy/generator/generator_test.py
+++ PythonStudy/generator/generator_test.py
@@ -1,11 +1,21 @@
+#!/usr/bin/env python
 '''
 Created on Jun 4, 2017
 
 @author: bob
 '''
 
-import this
+# import this
 
 if __name__ == '__main__':
-    n = ((a,b) for a in range(0,2) for b in range(4,6))
-    print n
+#     n = ((a,b) for a in range(0,2) for b in range(4,6))
+#     print n
+    
+    for i in xrange(1, 511):
+        if i in [252, 253, 254]:
+            continue
+        
+        if i < 256:
+            print "127.11.%d.x" % i
+        else:
+            print "127.10.%d.x" % (i - 255)
